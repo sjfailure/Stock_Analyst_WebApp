257e9062923166c778797e7cb83efd11f85022c0
Mos <sj_martin2023@hotmail.com>
2025-08-14 23:01:40 -0500

    Add run_setup.py for manage.py start up commands

--- mvp_stock_app/asgi.py
+++ mvp_stock_app/asgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mvp_stock_app.settings')
-
+call_command('run_setup')
 application = get_asgi_application()

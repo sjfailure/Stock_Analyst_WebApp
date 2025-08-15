bf78f9e8d53fd94ac166a40f0474a3d2cdbf0cbc
Mos <sj_martin2023@hotmail.com>
2025-08-15 00:31:59 -0500

    Add db pinger functionality

--- stock_analyst/urls.py
+++ stock_analyst/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('test', views.index, name='index'),
     path('', views.main, name='main'),
     path('data_stream', views.main_data_stream, name='data_stream'),
-    path('health-check/', views.db_health_check, name='health_check'),
+    path('health-check', views.db_health_check, name='health_check'),
 ]
